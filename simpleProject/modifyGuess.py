172038fcc56cfe73d23fb4bc440a9556d0ad3d50
Hyacinthe <80036233+miamo19@users.noreply.github.com>
2022-10-01 04:02:32 +0100

    Add functionality to guess number

---
+++ simpleProject/modifyGuess.py
@@ -0,0 +1,42 @@
+# computer will guess from a random values provider the user know the correct number
+import random
+
+
+def computer_guess(x):
+    high = x
+    low = 1
+    feedback = " "
+    n = 1
+    while feedback != "c":
+        if high != low:
+            guess = random.randint(low, high)
+            print(guess)
+        else:
+            guess = low
+
+        user_num = 3
+
+        if guess !=user_num:
+            feedback = input(f"Is {guess} too low , too high? ")
+            try:
+                if feedback == 'h'or feedback=='H':
+                    print("too high")
+                    high = guess-1
+                elif feedback == 'l' or feedback=='L':
+                    print("too low")
+                    low = guess+1
+            except TypeError as e:
+                print("Check the that your response is either 'h' or 'l' and try again", e)
+            if n==3:
+                print("You Fail!! You exhauted your trial.")
+                exit()
+            n+=1
+        else:
+            print("you Won, correct number")
+            exit()
+
+    print("you Won, correct number")
+
+
+
+computer_guess(10)
