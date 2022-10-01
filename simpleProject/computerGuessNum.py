49d9de7c08a57225bf1ec0a024ba28a03f3429cd
Hyacinthe <80036233+miamo19@users.noreply.github.com>
2022-10-01 04:00:21 +0100

    computer guess number

---
+++ simpleProject/computerGuessNum.py
@@ -0,0 +1,22 @@
+#computer will guess from a random values provider the user know the correct number
+import random
+
+def computer_guess(x):
+    high =x
+    low = 1
+    feedback = " "
+
+    while feedback != "c":
+        if high!=low:
+            guess = random.randint(low, high)
+
+        else:
+            guess = low
+
+        feedback = input(f"Is {guess} too high (h), or too low (l) or correct (c)?")
+        if feedback == 'h':
+            high = guess-1
+        elif feedback == 'l':
+            low = guess+1
+    print("you Won!! correct guess")
+computer_guess(10)
