65b1eeba66ec5589d45b97dc4330bd5eeda7b90d
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-27 02:55:17 +0100

    Update primeNum.py

--- simpleProject/primeNum.py
+++ simpleProject/primeNum.py
@@ -4,7 +4,7 @@ def prime():
     num = int(input("Enter the Number: "))
     flag = False
     if num>1:
-        #check foe factor
+        #check for factor
         for i in range(2,num):
             if (num%i==0):
                 #if factor is found: set flag to be True
