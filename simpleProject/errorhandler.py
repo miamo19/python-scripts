e13452131b285c18404da9392fe8465fb73cddeb
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-02-14 06:56:19 +0100

    Update errorhandler.py

--- simpleProject/errorhandler.py
+++ simpleProject/errorhandler.py
@@ -9,3 +9,16 @@ finally:
 fil = open('demo2.txt', 'r')
 print(fil.read())
 fil.close()
+
+
+#The above code for opening a file can be writen as below👇👇👇
+"""
+with open("demo2.txt", "w") as file:
+    try:
+    for i in range(5):
+        file.write('\tThe situation in the northwest and southwest is %d\n' %i)  #%i or %(i+1)
+    finally:
+        file.close()
+    with open("demo2.txt", "r") as fil:
+        print(fil.read())
+"""
