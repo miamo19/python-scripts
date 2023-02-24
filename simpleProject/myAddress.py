2996c1c987505169fa688542b99382a48f4f202f
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-02-24 05:09:53 +0100

    Update myAddress.py

--- simpleProject/myAddress.py
+++ simpleProject/myAddress.py
@@ -3,6 +3,7 @@ import json
 from urllib.request import urlopen
 import ipinfo as ipinfo
 
+#Provide the url
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
 data = json.loads(response.read())
