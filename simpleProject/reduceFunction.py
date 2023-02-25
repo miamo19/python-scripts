be7a249e4afd35d4ec11ae37b13449f50c083dd3
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-02-25 04:49:21 +0100

    Update reduceFunction.py

--- simpleProject/reduceFunction.py
+++ simpleProject/reduceFunction.py
@@ -1,35 +1,35 @@
 from functools import reduce
 
 list_a = [2, 3, 5, 4, 6, 7, 8]
-map_a = map(lambda x:list_a%2, list_a)
+map_a = map(lambda x:x%2, list_a)
 print(list(map_a))
 
 print("------------@---------------")
-y = [1, 2, 4, 7, 3, 5, 4]
+tuple_b = (1, 2, 4, 7, 3, 5, 4)
 
-map_list = map(lambda i, j: x + y, x,y)
+map_list = map(lambda i, j: i + j, list_a, tuple_b)
 print(map_list)
 print(list(map_list))
 
 print("-------------@--------------")
 
 #check for filter
-c = filter(lambda i:i%2==0, x)
-print(list(c))
+filter_c = filter(lambda i:i%2==0, list_a)
+print(list(filter_c))
 
 print("-------------@--------------")
 
 #Filter function uses conditional statment to iterate over the list
-d = filter(lambda i:True if i>=5 else False , x)
-print(list(d))
+filter_d = filter(lambda i:True if i>=5 else False , list_a)
+print(list(filter_d))
 
 print("-----------@---------------")
 
         #reduce fumction perform operation on elements in a list
 #The below reduce function add all the elements in a list
-z = reduce(lambda i,j:i+j, y)
-print(z)
+reduce_d = reduce(lambda i,j:i+j, tuple_b)
+print(reduce_d)
 print("-----------@----------------")
 
-m = reduce(lambda i,j:i-j, y)
-print(m)
+reduce_e = reduce(lambda i,j:i-j, tuple_b)
+print(reduce_e)
