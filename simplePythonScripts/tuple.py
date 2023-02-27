8fdf59ffbc317a20b98d409a3cc5ccd2881ca815
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-02-27 02:56:01 +0100

    Update tuple.py

--- simplePythonScripts/tuple.py
+++ simplePythonScripts/tuple.py
@@ -1,7 +1,11 @@
-  #tuple are just ad list but differ from list in that, tuples are immutable(cannot be change once created)
+#tuple are just as list but differ from list in that, tuples are immutable(cannot be change once created)
+#you cannot insert or remove anything from the tuple
+
 x= (1, 5, 3, 7, 5)
-y= ('Man', 5, 8, 'Hyacinth')         #it is possible to save multiple data types values in a tuple as list
-  #you cannot insert or remove anything from the tuple
+
+#it is possible to save multiple data types values in a tuple as list
+y= ('Man', 5, 8, 'Hyacinth')         
+ 
 
 print(x.count(5))       # count the number of 5 in the tuple
 print(len(y))           # display the number if element found in the tuple
