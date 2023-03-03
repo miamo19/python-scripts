191583cfb06425a42f4245fa0011cf0884fba6e9
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-03-03 03:33:49 +0100

    Update primeNum.py

--- simpleProject/primeNum.py
+++ simpleProject/primeNum.py
@@ -1,13 +1,12 @@
 #program to check if the  number enter is a prime number or not
 def prime():
-    #Take input value from user to check if value is prime number
-    num = int(input("Enter the Number: "))
+    num = int(input("Enter the Number: "))  #Take input value from user to check if value is prime number
     flag = False
+   
     if num>1:
-        #check for factor
-        for i in range(2,num):
+        for i in range(2,num):    #check for factor
             if (num%i==0):
-                #If the Factor is Found: set flag to be True and break out of the loop 
+                            #If the Factor is Found: set flag to be True and break out of the loop 
                 flag =True
                 break
     else:
