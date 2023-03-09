81323e6584466dd77fceaffbdee43f8667f6ed0b
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-03-09 10:28:17 +0100

    Update script0.py

--- simplePythonScripts/script0.py
+++ simplePythonScripts/script0.py
@@ -1,5 +1,6 @@
-#Finding the max Number
+#Finding the max Number among 3 sets of numbers
 numb1 = float(input("Enter first Number: "))
 numb2 = float(input("Enter second Number: "))
 numb3 = float(input("Enter third Number: "))
+
 print("The Max Value between {0}, {1} and {2} is {3} ".format(numb1,numb2,numb3,max(numb1,numb2,numb3)))
