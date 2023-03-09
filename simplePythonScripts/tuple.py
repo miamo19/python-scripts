c318ebc6ba8037092bf996f1168347115211fdd6
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-03-09 10:26:45 +0100

    Update tuple.py

--- simplePythonScripts/tuple.py
+++ simplePythonScripts/tuple.py
@@ -6,7 +6,6 @@ x= (1, 5, 3, 7, 5)
 #it is possible to save multiple data types values in a tuple as list
 y= ('Man', 5, 8, 'Hyacinth')         
  
-
 print(x.count(5))       # count the number of 5 in the tuple
-print(len(y))           # display the number if element found in the tuple
+print(len(y))           # display the number of element found in the tuple
 del(y)                  # delete the tuple y
