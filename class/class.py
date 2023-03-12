59918de32e9ac8523a4b7ab628b4183b93c388ba
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-03-12 06:04:07 +0100

    Update class.py

--- class/class.py
+++ class/class.py
@@ -3,6 +3,9 @@ class Rectangle:
         self.height = height
         self.width = width
         
+    def rect_area(self):
+        return self.height*self.width
+    
 # Instanciation (object of the Class Rectangle are created)
 rect1 = Rectangle(20, 12)
 rect2 = Rectangle(12, 9)
