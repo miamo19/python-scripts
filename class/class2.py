cd6e0e44b0d6ccbf3c53fc0fcb88359db365fc85
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-03-13 10:57:59 +0100

    Update class2.py

--- class/class2.py
+++ class/class2.py
@@ -2,7 +2,6 @@ class Market:
     def __init__(self, *arg, **arg1):
         print(arg)
         for k, v in arg1.items():
-            print(k,': ',v)
-
+            print(k, ': ', v)
 
 market1 = Market('yvan',25,'Hyasan',55, books=500, pens=100, pencils=25, Numbers='three')
