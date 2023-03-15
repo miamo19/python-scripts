7e69a5dfa1b19970d2756e3d080e97fb567dd638
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-03-15 04:10:04 +0100

    Update polygon.py

--- class/polygon.py
+++ class/polygon.py
@@ -1,14 +1,14 @@
  #the base class
 class Polygon:
     def __init__(self, no_of_sides):
-        self.n = no_of_sides
+        self.num_side = no_of_sides
         self.sides = [0 for i in range(no_of_sides)]
 
     def inputSides(self):
-        self.sides = [float(input("Enter side " + str(i + 1) + " : ")) for i in range(self.n)]
+        self.sides = [float(input("Enter side " + str(i + 1) + " : ")) for i in range(self.num_side)]
 
     def dispSides(self):
-        for i in range(self.n):
+        for i in range(self.num_side):
             print("Side", i + 1, "is", self.sides[i])
 
 # the inherited class
