65b3a5a9b2dd9cd435d43f0e6e29e396bc9278c4
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-03-17 05:33:29 +0100

    Update decorator.py

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -19,7 +19,9 @@ print("------------------------")
 
 @decorator
 def f2(x,y):
-    print("the sum of: ", x , " and ", y ," = ", x+y)
+    print("The sum of: ", x , " and ", y ," = ", x+y)
+#   print("The sum of: {} and {} is = {}".fomat(x,y,x+y))
+#   print("The sum of: %d and %d is = %s" %(x,y,x+y))
 
 f2(2, 8)
 
